e4902eb7cfc4daf465249c2a4dba654d091add86
Petrik Lajos Szg <info@petrik.hu>
2020-09-29 11:55:21 +0200

    Add project files.

---
+++ HarcosProjekt/Harcos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarcosProjekt
+{
+    class Harcos
+    {
+        string nev;
+        int szint;
+        int tapasztalat;
+        int eletero;
+        int alapEletero;
+        int alapSebzes;
+
+        
+    }
+}
