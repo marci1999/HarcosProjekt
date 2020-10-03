acc07c07759c5c7f4cb42b9e7f4a8a6b53933337
jakus marci <jakus.marton@gmail.com>
2020-10-03 17:54:23 +0200

    sajat harcos

--- HarcosProjekt/Program.cs
+++ HarcosProjekt/Program.cs
@@ -25,7 +25,19 @@ namespace HarcosProjekt
             Console.WriteLine(h1.Eletero);
             h1.Gyogyul();
             Console.WriteLine(h1.Eletero);*/
-
+            Console.WriteLine("Adjon meg a harcos nevet");
+            string nev = Console.ReadLine();
+            Console.WriteLine("és egy sablonszámot\n" +
+            "1: alapEletero = 15 alapSebzes = 3\n" +
+            "2: alapEletero = 12 alapSebzes = 4\n" +
+            "3: alapEletero = 8 alapSebzes = 5");
+            int sablonSzam = Convert.ToInt32(Console.ReadLine());
+            Harcos harcosom = new Harcos(nev, sablonSzam);
+            Console.WriteLine(harcosom);
+            do
+            {
+                
+            } while (true);
 
             Console.ReadLine();
         }
